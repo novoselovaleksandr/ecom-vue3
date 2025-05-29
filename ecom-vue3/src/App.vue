66ef3aa4a4258662d15747757ced9ba5fa52bd30
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2025-05-29 23:26:24 +0500

    [main] test tailwind and clean project

--- ecom-vue3/src/App.vue
+++ ecom-vue3/src/App.vue
@@ -1,21 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 import './assets/main.css'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <h1 class="text-xl font-bold underline">
+      Hello world!
+    </h1>
   </header>
 
   <RouterView />
