e23d9f125d7649e7b56aa5ea1106633964e5e588
Novoselov Aleksandr <novoselov.aleksandr.96@mail.ru>
2025-05-30 12:56:32 +0500

    [main] шаблоны страниц

---
+++ ecom-vue3/src/views/ProductView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Страница товара
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
